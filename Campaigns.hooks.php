5417cb5955e0bcaad04d97353268840059d4ac75
Reedy <reedy@wikimedia.org>
2017-10-12 01:12:00 +0100

    Remove pre-authmanager code
    Bug: T178024
    Change-Id: I79d69aa1cb34d69f9883aac43a6ed15a4c82e1fb

--- Campaigns.hooks.php
+++ Campaigns.hooks.php
@@ -1,23 +1,6 @@
 <?php
 
 class CampaignsHooks {
-	public static function onRegistration() {
-		global $wgHooks, $wgDisableAuthManager, $wgAuthManagerAutoConfig;
-
-		if ( class_exists( MediaWiki\Auth\AuthManager::class ) && empty( $wgDisableAuthManager ) ) {
-			$wgAuthManagerAutoConfig['secondaryauth'] += [
-				CampaignsSecondaryAuthenticationProvider::class => [
-					'class' => CampaignsSecondaryAuthenticationProvider::class,
-					'sort' => 0, // non-UI secondaries should run early
-				]
-			];
-			$wgHooks['AuthChangeFormFields'][] = 'CampaignsHooks::onAuthChangeFormFields';
-		} else {
-			$wgHooks['UserCreateForm'][] = 'CampaignsHooks::onUserCreateForm';
-			$wgHooks['AddNewAccount'][] = 'CampaignsHooks::onAddNewAccount';
-		}
-	}
-
 	public static function onAuthChangeFormFields(
 		array $requests, array $fieldInfo, array &$formDescriptor, $action
 	) {
@@ -25,67 +8,5 @@ class CampaignsHooks {
 			$formDescriptor['createOrLogin']['linkQuery'] .=
 				( $formDescriptor['createOrLogin']['linkQuery'] ? '&' : '' ) . 'campaign=loginCTA';
 		}
-	}
-
-	public static function onUserCreateForm( &$template ) {
-		$maxCampaignLen = 40;
-
-		$skin = $template->getSkin();
-		$request = $skin->getRequest();
-		$campaign = $request->getVal( 'campaign', '' );
-		if ( $campaign === '' || strlen( $campaign ) > $maxCampaignLen ) {
-			return true;
-		}
-
-		// The version of the page for a logged-in user will not be cached and
-		// served to anonymous users, so it's OK to check on server.
-		if ( !$skin->getUser()->isAnon() ) {
-			return true;
-		};
-
-		$template->set( 'header',
-			Html::hidden( 'campaign', $campaign )
-		);
-
-		return true;
-	}
-
-	public static function onAddNewAccount( $user, $byEmail ) {
-		global $wgRequest, $wgUser;
-
-		$userId = $user->getId();
-		$creatorUserId = $wgUser->getId();
-
-		// MediaWiki allows existing users to create accounts on behalf
-		// of others. In such cases the ID of the newly-created user and
-		// the ID of the user making this web request are different.
-		$isSelfMade = ( $userId && $userId === $creatorUserId );
-
-		$displayMobile = class_exists( 'MobileContext' ) &&
-			MobileContext::singleton()->shouldDisplayMobileView();
-
-		$event = [
-			'userId' => $userId,
-			'userName' => $user->getName(),
-			'isSelfMade' => $isSelfMade,
-			'campaign' => $wgRequest->getVal( 'campaign', '' ),
-			'displayMobile' => $displayMobile,
-			// @todo: Remove these unused fields when they're no longer required by the schema.
-			'token' => '',
-			'userBuckets' => '',
-		];
-
-		$returnTo = $wgRequest->getVal( 'returnto' );
-		if ( $returnTo !== null ) {
-			$event[ 'returnTo' ] = $returnTo;
-		}
-
-		$returnToQuery = $wgRequest->getVal( 'returntoquery' );
-		if ( $returnToQuery !== null ) {
-			$event[ 'returnToQuery' ] = $returnToQuery;
-		}
-
-		EventLogging::logEvent( 'ServerSideAccountCreation', 5487345, $event );
-		return true;
 	}
 }

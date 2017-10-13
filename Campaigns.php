2f4aebce06b8be5600cd45e65290d67272e54ef0
James D. Forrester <jforrester@wikimedia.org>
2017-10-13 14:19:28 -0700

    Uncomment wfWarn in deprecated PHP entry point
    Change-Id: I2d46590f22b9f944a1be94804beeea00d381d7aa

--- Campaigns.php
+++ Campaigns.php
@@ -3,10 +3,10 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 	wfLoadExtension( 'Campaigns' );
 	// Keep i18n globals so mergeMessageFileList.php doesn't break
 	$wgMessagesDirs['Campaigns'] = __DIR__ . '/i18n';
-	/* wfWarn(
+	wfWarn(
 		'Deprecated PHP entry point used for Campaigns extension. Please use wfLoadExtension instead, ' .
 		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
-	); */
+	);
 	return;
 } else {
 	die( 'This version of the Campaigns extension requires MediaWiki 1.25+' );

b6a47cffdf98516a2a354d9fa895f4c8e59fa5ef
Gergő Tisza <tgr.huwiki@gmail.com>
2025-02-06 18:05:38 +0100

    Preserve 'campaign' parameter during authentication
    Change-Id: I596545a1987be03b2a895259d589acd087d73a1e

--- includes/Hooks.php
+++ includes/Hooks.php
@@ -2,9 +2,14 @@
 
 namespace MediaWiki\Extension\Campaigns;
 
+use MediaWiki\Auth\Hook\AuthPreserveQueryParamsHook;
+use MediaWiki\Context\RequestContext;
 use MediaWiki\SpecialPage\Hook\AuthChangeFormFieldsHook;
 
-class Hooks implements AuthChangeFormFieldsHook {
+class Hooks implements
+	AuthChangeFormFieldsHook,
+	AuthPreserveQueryParamsHook
+{
 	/** @inheritDoc */
 	public function onAuthChangeFormFields(
 		$requests, $fieldInfo, &$formDescriptor, $action
@@ -13,5 +18,11 @@ class Hooks implements AuthChangeFormFieldsHook {
 			$formDescriptor['createOrLogin']['linkQuery'] .=
 				( $formDescriptor['createOrLogin']['linkQuery'] ? '&' : '' ) . 'campaign=loginCTA';
 		}
+	}
+
+	/** @inheritDoc */
+	public function onAuthPreserveQueryParams( array &$params, array $options ) {
+		$request = RequestContext::getMain()->getRequest();
+		$params['campaign'] = $request->getRawVal( 'campaign' );
 	}
 }
